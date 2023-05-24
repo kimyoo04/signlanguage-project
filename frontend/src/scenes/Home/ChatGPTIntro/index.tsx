10c59c3ccc60746f11bbbaddacd6cb422398f4d9
Kim Yoo <kimyoo04eco@naver.com>
2023-05-25 00:22:52 +0900

    css: 홈페이지 UI 수정

--- frontend/src/scenes/Home/ChatGPTIntro/index.tsx
+++ frontend/src/scenes/Home/ChatGPTIntro/index.tsx
@@ -4,35 +4,31 @@ export default function ChatGPTIntro() {
   return (
     <div className="col-center py-8">
       <div className="flex w-full border-b pb-2">
-        <h1 className="text-3xl font-medium">챗봇 서비스</h1>
+        <h1 className="text-2xl font-medium">챗봇 서비스</h1>
       </div>
 
-      <div className="row-center py-8">
-        <Image
-          src={"/images/chatgpt_logo.png"}
-          width={110}
-          height={90}
-          alt={"chatgpt_logo"}
-        />
-        <Image
-          src={"/images/bardai.png"}
-          width={500}
-          height={200}
-          alt={"bardai"}
-        />
-      </div>
+      <div className="col-center w-full gap-8 py-8 xl:flex-row xl:justify-between xl:gap-32">
+        <div className="row-center h-[70px] w-80 gap-2 py-8">
+          <Image
+            src={"/images/chatgpt_logo.png"}
+            width={70}
+            height={70}
+            alt={"chatgpt_logo"}
+          />
+          <span className="text-4xl font-bold text-main_color">+</span>
+          <Image
+            src={"/images/bardai.png"}
+            width={270}
+            height={200}
+            alt={"bardai"}
+          />
+        </div>
 
-      <div>
         <p>
           ChatGPT는 OpenAI가 제공하는 서비스, Bard는 Google에서 제공하는
-          서비스입니다.
-        </p>
-        <p>
-          ChatGPT API와 Bard API는 개발자들이 대화형 인공지능 모델에 액세스하여
-        </p>
-        <p>
-          다양한 애플리케이션과 서비스에서 자연어 처리 기능을 활용할 수 있도록
-          도와줍니다.
+          서비스입니다. ChatGPT API와 Bard API는 개발자들이 대화형 인공지능
+          모델에 액세스하여 다양한 애플리케이션과 서비스에서 자연어 처리 기능을
+          활용할 수 있도록 도와줍니다.
         </p>
       </div>
     </div>
--- frontend/src/scenes/Home/SignLanguageIntro/index.tsx
+++ frontend/src/scenes/Home/SignLanguageIntro/index.tsx
@@ -4,11 +4,11 @@ export default function SignLanguageIntro() {
   return (
     <div className="col-center py-8">
       <div className="flex w-full border-b pb-2">
-        <h1 className="text-3xl font-medium">수화 번역 서비스</h1>
+        <h1 className="text-2xl font-medium">수화 번역 서비스</h1>
       </div>
 
-      <div className="flex w-full flex-col items-start justify-center py-8">
-        <div className="flex w-full flex-col items-start">
+      <div className="flex w-full flex-col items-center justify-center py-8">
+        <div className="flex w-full flex-col items-center">
           <p>4조의 수어 번역 서비스는 혁신적인 방식을 사용합니다.</p>
           <p>▶ 이미지를 입력하면 mlflow의 모델을 통해 알파벳을 인식합니다.</p>
           <p>
@@ -21,12 +21,39 @@ export default function SignLanguageIntro() {
           </p>
         </div>
 
-        <Image
-          src={"/images/수어번역서비스 모델 설명.png"}
-          width={700}
-          height={700}
-          alt={"수어번역서비스 모델 설명"}
-        />
+        <div className="pt-8">
+          <Image
+            src={"/images/수어번역서비스 모델 설명.png"}
+            width={1024}
+            height={1024}
+            alt={"수어번역서비스 모델 설명"}
+            className="w-full"
+          />
+
+          <div className="grid grid-cols-4 gap-3">
+            <div className="flex items-start justify-center rounded-lg border p-3">
+              <span className="text-center text-sm">
+                1. 수어 이미지 파일 업로드
+              </span>
+            </div>
+            <div className="flex items-start justify-center rounded-lg border p-3">
+              <span className="text-center text-sm">
+                2. CNN 모델을 통한 이미지 분류 mlflow : 머신러닝 프로젝트의
+                생명주기 관리를 위한 오픈 소스 플랫폼
+              </span>
+            </div>
+            <div className="flex items-start justify-center rounded-lg border p-3">
+              <span className="text-center text-sm">
+                3.ChatGPT API, BARD API 응답 요청
+              </span>
+            </div>
+            <div className="flex items-start justify-center rounded-lg border p-3">
+              <span className="text-center text-sm">
+                4. 응답 결과 사용자에게 전송
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
