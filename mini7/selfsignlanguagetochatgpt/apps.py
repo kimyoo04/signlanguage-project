0d7bfb08c7e949741c3e5781a7da3bd6eb7c6fae
wath1457 <wath1457@gmail.com>
2023-05-24 14:13:48 +0900

    feat: add signLanguage translate

--- mini7/selfsignlanguagetochatgpt/apps.py
+++ mini7/selfsignlanguagetochatgpt/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class SelfsignlanguagetochatgptConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'selfsignlanguagetochatgpt'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "selfsignlanguagetochatgpt"
