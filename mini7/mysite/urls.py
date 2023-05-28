d2d378ec7d28dfcc44dee4e454ca91c16c042f5d
Kim Yoo <kimyoo04eco@naver.com>
2023-05-28 14:22:47 +0900

    fix: redoc 설정 제거, url에서 슬래시 위치 조정

--- mini7/mysite/urls.py
+++ mini7/mysite/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
 
-from rest_framework import routers, permissions
+from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -19,22 +19,20 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
 
     # API 문서
     path('api/schema',
          schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('api/swagger', schema_view.with_ui('swagger',
                                             cache_timeout=0), name='schema-swagger-ui'),
-    path('api/redoc/', schema_view.with_ui('redoc',
-                                           cache_timeout=0), name='schema-redoc'),
 
     # API URL
-    path('api/', include([
-        path('chat/', include('selfchatgpt.urls')),
-        path('signLanguage/', include('selfsignlanguagetochatgpt.urls')),
-        path('posts/', include('posts.urls')),
-        path('account/', include('account.urls')),
+    path('api', include([
+        path('/chat', include('selfchatgpt.urls')),
+        path('/signLanguage', include('selfsignlanguagetochatgpt.urls')),
+        path('/posts', include('posts.urls')),
+        path('/account', include('account.urls')),
     ]))
 ]
 
