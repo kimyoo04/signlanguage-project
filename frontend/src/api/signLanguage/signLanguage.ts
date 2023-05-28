67a690a64a54a1a52e1c814c52b6a06f335745f9
Kim Yoo <kimyoo04eco@naver.com>
2023-05-28 17:26:39 +0900

    fix: 수어 이미지 요청 함수에 FormData 타입으로 수정

--- frontend/src/api/signLanguage/signLanguage.ts
+++ frontend/src/api/signLanguage/signLanguage.ts
@@ -1,17 +1,12 @@
-import axios from "axios";
+import axios from "@api/axiosInstance";
 
-export const signLanguage = async (files: string[]) => {
-  console.log(files);
+export const signLanguage = async (files: FormData) => {
   try {
-    const response = await axios.post(
-      "http://localhost:3000/api/signLanguage",
-      { files },
-      {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      }
-    );
+    const response = await axios.post("/signLanguage", files, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
     return response.data;
   } catch (err) {
     console.error(err);
