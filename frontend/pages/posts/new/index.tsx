4bdd982211476bf65e8df9178d319f9fe16e8783
Kim Yoo <kimyoo04eco@naver.com>
2023-05-28 17:12:17 +0900

    css: 게시글 생성 페이지 레이아웃 조정

--- frontend/pages/posts/new/index.tsx
+++ frontend/pages/posts/new/index.tsx
@@ -1,10 +1,10 @@
-import MainLayout from "@layouts/MainLayout";
+import OnePageLayout from "@layouts/OnePageLayout";
 import AddPost from "@scenes/Posts/AddPost";
 
 export default function CreatePostPage() {
   return (
-    <MainLayout>
+    <OnePageLayout>
       <AddPost />
-    </MainLayout>
+    </OnePageLayout>
   );
 }
